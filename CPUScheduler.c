003bc96f5ebbca1d288efe7dd136af064bd783af
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-05-22 22:09:16 +0300

    comments and function prototypes has been added.

--- CPUScheduler.c
+++ CPUScheduler.c
@@ -3,27 +3,41 @@
 #include <string.h>
 #include <stdarg.h>
 
+// Define constants for maximum number of processes, RAM size, reserved RAM, and time quantums
 #define MAX_PROCESSES 100
 #define RAM_SIZE 2048
 #define RESERVED_RAM_FOR_PRIORITY_0 512
 #define TIME_QUANTUM_8 8
 #define TIME_QUANTUM_16 16
 
+// Process struct definition
 typedef struct {
-    char id[10];
-    int arrival_time;
-    int priority;
-    int burst_time;
-    int ram;
-    int cpu_rate;
+    char id[10];                 // Process ID
+    int arrival_time;            // Arrival time of the process
+    int priority;                // Priority of the process
+    int burst_time;              // Burst time of the process
+    int ram;                     // RAM required by the process
+    int cpu_rate;                // CPU rate required by the process
 } Process;
 
+// Queue struct definition
 typedef struct {
-    Process processes[MAX_PROCESSES];
-    int count;
+    Process processes[MAX_PROCESSES];   // Array of processes in the queue
+    int count;                          // Number of processes in the queue
 } Queue;
 
+// ProcessLog struct definition
 typedef struct {
-    char ids[MAX_PROCESSES][10];
-    int count;
+    char ids[MAX_PROCESSES][10];    // Array of process IDs for logging
+    int count;                      // Number of logged processes
 } ProcessLog;
+
+// Function prototypes
+void read_input_file(char *filename, Queue *queue);
+void allocate_processes(Queue *input_queue, Queue *cpu1_queue, Queue *cpu2_queues[]);
+void simulate_fcfs(Queue *cpu_queue, char *cpu_name, int *cpu_ram, int *cpu_rate, ProcessLog *log);
+void simulate_sjf(Queue *cpu_queue, char *cpu_name, int *cpu_ram, int *cpu_rate, ProcessLog *log);
+void simulate_round_robin(Queue *cpu_queue, char *cpu_name, int time_quantum, int *cpu_ram, int *cpu_rate, ProcessLog *log);
+void log_process_state(const char *format, ...);
+void print_process_logs(ProcessLog cpu1_log, ProcessLog cpu2_logs[]);
+int check_resources(int required_ram, int available_ram, int required_cpu_rate, int available_cpu_rate);
